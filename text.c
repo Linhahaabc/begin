695c86c2ce256e2a917d8db610ae6d2c80d9aa6d
哈哈哈 <1714302891@qq.com>
2024-07-28 17:04:43 +0800

    7.28.1

--- text.c
+++ text.c
@@ -1,12 +1,15 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+//计算你年龄天数
 #include<stdio.h>
-int main()
+int main(void)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	scanf("%d %d", &a, &b);
-	c = a*b;
-	printf("c=%d\n", c);
+	int year, month, day, allday;
+	year = 0;
+	month = 0;
+	day = 0;
+	allday = 0;
+	scanf("%d %d %d", &year, &month, &day);
+	allday = allday + year * 365 + month * 30 + day;
+	printf("年龄天数是%d\n", allday);
 	return 0;
 }
