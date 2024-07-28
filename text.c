f04fe5ac2d1f7305e6335d7fa70eb4fa25923427
哈哈哈 <1714302891@qq.com>
2024-07-28 17:03:25 +0800

    7.27.1

--- text.c
+++ text.c
@@ -1,7 +1,12 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 int main()
 {
-	printf("有点意思啊\n");
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf(" %d %d", &a, &b);
+	c = a + b;
+	printf("c=%d\n", c);
 	return 0;
 }
-//快捷键；ctrl+f5
