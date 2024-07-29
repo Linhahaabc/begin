869a069eba676ac0f5c2aff85af7b6b4f9fbfbee
哈哈哈 <1714302891@qq.com>
2024-07-29 18:03:02 +0800

    7.29.4

--- text.c
+++ text.c
@@ -1,26 +1,19 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-/*初始函数*/
-void one_three(void);
-void two(void);
-/*主函数*/
 int main(void)
 {
-	printf("starting now:\n");
-	one_three();
-	printf("\n");
-    two();
-	printf("\n");
-	printf("three\n");
-	printf("done\n");
-	return 0;
-}
-/*定义函数*/
-void one_three(void)
-{
-	printf("one");
-}
-void two(void)
-{
-	printf("two");
+	float weight;
+	float value;
+	printf("Are you worth you weight in platinum\n");
+	printf("Let's check in out\n");
+	printf("Please enter you weight in pound:");
+	scanf("%f",&weight);
+	value = 1700 * weight * 14.5833;
+	printf("You weight in platinum is worth $%f\n",value);
+	printf("You are easliy worthn that,If platinum prices drop,\n");
+	printf("eat more to maintain you value\n");
+	return 0; 
+
+
+
 }
