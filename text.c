c0eb085f3e6d84a9febdd82b5e731a544b11ca24
哈哈哈 <1714302891@qq.com>
2024-07-29 13:00:37 +0800

    7.29.2

--- text.c
+++ text.c
@@ -1,28 +1,16 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-/*多函数运用与同行函数的分行*/
 #include<stdio.h>
-/*函数原型*/
-void br(void);
-void ic(void);
-/*内容*/
+void ix(void);
 int main(void)
 {
-	br();
-	printf(",");
-	ic();
+	ix(), ix(), ix();
 	printf("\n");
-	ic();
+	ix(), ix();
 	printf("\n");
-	br();
+	ix();
 	return 0;
-	
 }
-/*函数的定义*/
-void br(void)
+void ix(void)
 {
-	printf("Brazil,Russia");
-}
-void ic(void)
-{
-	printf("India,China");
+	printf("smile!");
 }
