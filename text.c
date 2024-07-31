bcd5cb234fa75ff8b991c8214699c90e37d19426
哈哈哈 <1714302891@qq.com>
2024-07-31 18:30:13 +0800

    7.30.1

--- text.c
+++ text.c
@@ -2,8 +2,13 @@
 #include<stdio.h>
 int main(void)
 {
-	int a = 100;
-	printf("a=%d,a=%o,a=%x\n", a, a, a);
-	printf("a=%d,a=%#o,a=%#x",a, a, a);
+	int a = 65537;
+	long b = 1000000000;
+	long long c = 1000000000000000000;
+	unsigned long long d = 3000000000000;
+	printf("a=%x,a=%lx\n",a,a);      /*十六进制下65537*/
+	printf("b=%ld,b=%hu\n", b, b);   /*long的用法*/
+	printf("c=%lld,c=%d\n", c, c);   /*long long的用法*/
+	printf("d=%llu,d=%hd\n", d, d);  /*unsigned的用法*/
 	return 0;
 }
