988756ef0fbeadfd30589b11b58a2f9c8956ac31
哈哈哈 <1714302891@qq.com>
2024-07-31 18:42:11 +0800

    7.31.3

--- text.c
+++ text.c
@@ -1,14 +1,16 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<inttypes.h>
-/*可移植整数类型名*/
+/*超出float最大限度的错误*/
 int main(void)
 {
-	int32_t ak;
-	printf("请输入八位数字\n");
-	scanf("%ld", &ak);
-	printf("ak=%"PRId32"\n", ak);
-	printf("\"a\\is a backslash.\"\n");
+/*float精度为六位有效数字*/
+	float b = 1.0e20;
+	float c = 0;
+	c = b + 1;
+	b = c - 1;
+	/*16进制的的浮点写法*/
+	float d = 0x2.0p20;
+	printf("b=%f,c=%f\n", b, c);
+	printf("d=%llu,d=%x",d,d);
 	return 0;
-
 }
