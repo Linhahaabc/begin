142f168f621221a7c6fc0a4e7cb0c9848ae40a53
哈哈哈 <1714302891@qq.com>
2024-07-31 18:31:27 +0800

    7.31.1

--- text.c
+++ text.c
@@ -1,14 +1,11 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+/*字符的代号编码*/
 #include<stdio.h>
 int main(void)
 {
-	int a = 65537;
-	long b = 1000000000;
-	long long c = 1000000000000000000;
-	unsigned long long d = 3000000000000;
-	printf("a=%x,a=%lx\n",a,a);      /*十六进制下65537*/
-	printf("b=%ld,b=%hu\n", b, b);   /*long的用法*/
-	printf("c=%lld,c=%d\n", c, c);   /*long long的用法*/
-	printf("d=%llu,d=%hd\n", d, d);  /*unsigned的用法*/
+	char ak;
+	scanf("%c", &ak);
+	printf("the %c is %d", ak, ak);
 	return 0;
+
 }
