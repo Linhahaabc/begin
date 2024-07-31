4fb6d6442809472cdeda7079f98e507f56b4e23d
哈哈哈 <1714302891@qq.com>
2024-07-31 18:32:21 +0800

    7.31.2

--- text.c
+++ text.c
@@ -1,11 +1,14 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-/*字符的代号编码*/
 #include<stdio.h>
+#include<inttypes.h>
+/*可移植整数类型名*/
 int main(void)
 {
-	char ak;
-	scanf("%c", &ak);
-	printf("the %c is %d", ak, ak);
+	int32_t ak;
+	printf("请输入八位数字\n");
+	scanf("%ld", &ak);
+	printf("ak=%"PRId32"\n", ak);
+	printf("\"a\\is a backslash.\"\n");
 	return 0;
 
 }
