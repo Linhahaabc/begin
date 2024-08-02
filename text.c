2de13662bf241b1a383c2f1d59591ca1113a5790
哈哈哈 <1714302891@qq.com>
2024-08-02 15:08:56 +0800

    8.1.4

--- text.c
+++ text.c
@@ -1,14 +1,14 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-/*年龄转化为秒数*/
 int main(void)
 {
-	int a;
-	printf("请输入你的年龄:");
-	printf("________\b\b\b\b\b\b\b\b");
-	scanf("%d",&a);
-	printf("你的年龄一共有%lld秒", a * 31560000);
+	printf("已知一夸脱为950克，现在计算水分子的数量\n");
+	printf("请输入水分子的夸脱数");
+	float a,b;
+	printf("_______\b\b\b\b\b\b");
+	scanf("%f",&a);
+	b = a / 950 / 3.0e-30;
+	printf("%.2f夸脱水有%e水分子\n", a, b);
 	return 0;
-	
 
 }
