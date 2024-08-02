588146d9eb4296c818b550caf83d994d6d267410
哈哈哈 <1714302891@qq.com>
2024-08-02 15:09:13 +0800

    8.1.5

--- text.c
+++ text.c
@@ -1,14 +1,11 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#define PRAISE "you are an extraordinary being."
 int main(void)
 {
-	printf("已知一夸脱为950克，现在计算水分子的数量\n");
-	printf("请输入水分子的夸脱数");
-	float a,b;
-	printf("_______\b\b\b\b\b\b");
-	scanf("%f",&a);
-	b = a / 950 / 3.0e-30;
-	printf("%.2f夸脱水有%e水分子\n", a, b);
+	char name[40];
+	printf("What's your name?");
+	scanf("%s", name);
+	printf("Hello,%s.%s\n", name, PRAISE);
 	return 0;
-
 }
