863d1dfe27a33e9db19b253c4adf3291d8ee0769
哈哈哈 <1714302891@qq.com>
2024-08-02 15:07:22 +0800

    8.1.1

--- text.c
+++ text.c
@@ -1,16 +1,12 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-/*超出float最大限度的错误*/
 int main(void)
 {
-/*float精度为六位有效数字*/
-	float b = 1.0e20;
-	float c = 0;
-	c = b + 1;
-	b = c - 1;
-	/*16进制的的浮点写法*/
-	float d = 0x2.0p20;
-	printf("b=%f,c=%f\n", b, c);
-	printf("d=%llu,d=%x",d,d);
+	printf("Type int has a size of %zd bytes\n", sizeof(int));
+	printf("Type char has a size of %zd bytes\n", sizeof(char));
+	printf("Type long has a size of %zd bytes\n", sizeof(long));
+	printf("Type long long has a size of %zd bytes\n", sizeof(long long));
+	printf("Type double has a size of %zd bytes\n", sizeof(double));
+	printf("Type long double has a size of %zd bytes\n", sizeof(long double));
 	return 0;
 }
