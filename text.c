3e36f022eef93c09ce0d28aa6a323364e079ed8c
哈哈哈 <1714302891@qq.com>
2024-08-03 19:29:56 +0800

    8.3.3

--- text.c
+++ text.c
@@ -1,11 +1,12 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#define a "Hello world!"
+/*×Ö·û´®µÄ×ª»»*/
 int main(void)
 {
-	const int b = 6;
-	printf("%x %#x\n", 42, 42);
-	printf("*%d**% d**% d**\n", 42,42, -42);
-	printf("**%5d**%.3d**%5.3d**%05.3d\n", b, b, b, b);
+	printf("%2s\n", a);
+	printf("%20s\n", a);
+	printf("%20.3s\n", a);
+	printf("%-20s\n", a);
 	return 0;
-
 }
