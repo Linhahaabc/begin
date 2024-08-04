bb0f299779b8fcd14b080a006085ec890a3fff04
哈哈哈 <1714302891@qq.com>
2024-08-04 16:03:11 +0800

    8.4.2

--- text.c
+++ text.c
@@ -1,16 +1,1 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-int main(void)
-{
-	unsigned width, precision;
-	int nummer=0;
-	double weight=0;
-	printf("Enter a filed width:\n");
-	scanf("%d",&width);
-	printf("The nummer is :%-*d:\n", width, nummer);
-	scanf("%d %d",&width,&precision);
-	printf("Weight=%*.*f\n", width, precision, weight);
-	printf("Done!");
-	return 0;
-
-}
