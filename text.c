e08750c4247f04ffc5075266552e9556fa658c6c
哈哈哈 <1714302891@qq.com>
2024-08-04 16:05:22 +0800

    8.4.5

--- text.c
+++ text.c
@@ -1,11 +1,11 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#define BOOK "War and Peace"
 int main(void)
 {
-	float cost = 12.99;
-	float percent = 80.0;
-	printf("This copy of \"%s\" sells for %.2f.\n", BOOK, cost);
-	printf("That is %.f%% of list.", percent);
+	char NAME[20],SURNAME[20];
+	
+	printf("请输入你的名和姓\n");
+	scanf("%s %s", NAME, SURNAME);
+	printf("%s,%s\n", NAME, SURNAME);
 	return 0;
 }
