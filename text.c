896c18c7b9bc34704c132bfc10e6d328f944b788
哈哈哈 <1714302891@qq.com>
2024-08-04 16:04:09 +0800

    8.4.3

--- text.c
+++ text.c
@@ -1,1 +1,11 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+/*scanf中*的运用*/
+int main(void)
+{
+	int a;
+	printf("Please enter these integer:\n");
+	scanf("%*d %*d %d", &a);
+	printf("The last integer was %d", a);
+	return 0;
+}
