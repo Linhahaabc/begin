45544fb9ac14d529a93b08f6d8cfb44be48428ee
哈哈哈 <1714302891@qq.com>
2024-08-07 15:29:03 +0800

    8.6.1

--- text.c
+++ text.c
@@ -1,14 +1,1 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-int main(void)
-{
-	char NAME[10], SURNAME[10];
-	float height;
-	printf("Please enter your suiname and your name.\n");
-	scanf("%s %s", SURNAME,NAME);
-	printf("Please enter your height(cm).\n");
-	scanf("%f",&height);
-	printf("%s%s,you are %.2f meter tall", SURNAME, NAME, height/100);
-	return 0;
-	
-}
