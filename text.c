b4a256347a84e260ee5345e0bc21a90221622001
哈哈哈 <1714302891@qq.com>
2024-08-07 15:29:19 +0800

    8.6.2

--- text.c
+++ text.c
@@ -1,1 +1,14 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char name[10], surname[10];
+	printf("Please enter your surname and name\n");
+	scanf("%s ",surname);
+	scanf("%s", name);
+    printf("%*s %*s\n", strlen(surname),surname, strlen(name),name);
+	printf("%*zd %*zd\n", strlen(surname), strlen(surname), strlen(name), strlen(name));
+	
+	return 0;
+}
