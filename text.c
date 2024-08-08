566e1b49724ee98496cd4d72853a216075dc9d44
哈哈哈 <1714302891@qq.com>
2024-08-08 15:36:52 +0800

    8.8.2

--- text.c
+++ text.c
@@ -1,17 +1,19 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
+#define FORCE 1
 #include<stdio.h>
-/*输入一个数字，打印之后10个数字*/
 int main(void)
 {
-	int num1,num2,num3;
-	printf("Please enter a number\n");
-	scanf("%d", &num1);
-	num2 = num1 - 1;
-	num3 = num1 + 10;
-	while (num2++ < num3)
+	int first, second;
+	scanf("%d", &second);
+	scanf("%d", &first);
+	while (first > 0)
 	{
-		printf("%d\n", num2);
+		printf("%d %% %d is %d\n", first, second, first % second);
+		scanf("%d",&first);
+
+
 	}
-		
+	printf("done!\n");
 	return 0;
 }
+
