cc100b995f7198a02ea598765c0b0f549120b2a6
哈哈哈 <1714302891@qq.com>
2024-08-09 15:58:15 +0800

    8.9.1

--- text.c
+++ text.c
@@ -2,21 +2,13 @@
 #include<stdio.h>
 int main(void)
 {
-	int line,cubic,max_line;
-	max_line = 10;
-
-	printf(" line  num cubic\n");
-	for (line = 1; line <= max_line; line++)
-	{
-		
-		
-		cubic = line * line* line;
-		printf("%5d %5d %5d\n", line,line,cubic);
-
-	}
-	printf("Done!");
+	int n, x;
+	printf("Enter a intger:");
+	scanf("%d",&n);
+	for (x = 4; n <= 100000000; n = x * n);
+	printf("%d", n);
 	return 0;
+	
 
-
+	
 }
-
