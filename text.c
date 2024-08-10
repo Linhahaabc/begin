8e2e3cee4c0a11c4d0aacd5c766895c93eb84367
哈哈哈 <1714302891@qq.com>
2024-08-10 17:31:20 +0800

    8.10.4

--- text.c
+++ text.c
@@ -1,16 +1,17 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+/*½ð×ÖËþÊý×Ö*/
 int main(void)
 {
-	int line,real_line,real_row;
-	printf("Please enter a intger:");
-	scanf("%d", &line);
-	for (real_line = 1; real_line <= line; real_line++)
+	int number,line,order,line1;
+	printf("Please enter a line(intger):");
+	scanf("%d", &number);
+	for (line ='A', order = 0, line1 = 1; line <= ('A'+number); order++)
 	{
-		for (real_row = real_line; real_row >= 1; real_row--)
-			printf("$");
+		
+		for ((int)line1=(int)line; line <=(char) order + (char)line1; line++)
+			printf("%c", line);
 		printf("\n");
 	}
 	return 0;
-	
 }
