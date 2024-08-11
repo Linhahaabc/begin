e53485470401615930cc9f6114c88d376ff69d44
哈哈哈 <1714302891@qq.com>
2024-08-11 20:00:50 +0800

    8.11.7

--- text.c
+++ text.c
@@ -2,25 +2,15 @@
 #include<stdio.h>
 int main(void)
 {
-	float input[8], sum[8];
-	int order;
-	printf("Please enter eight number:");
-	for (order = 0; order < 8; order++)
-		scanf("%f", &input[order]);
-
-	
-		for (order = 0; order < 8; order++)
-			printf("%.2f ", input[order]);
-		printf("\n");
-		sum[0] = input[0];
-		printf("%.2f ", sum[0]);
-		for (order = 1; order < 8; order++)
-		{
-			sum[order] = sum[order - 1] + input[order];
-			printf("%.2f ", sum[order]);
-		}
-		scanf("%.2f", &input[order]);
-	
-	printf("\nDone!");
+	char input[250];
+	int order,num,length;
+	printf("Please enter the letter(<=256):");
+	order = 0;
+	do {
+		scanf("%c", &input[order]);
+		
+	} while (input[order] != '\n' && ++order);/*&& ++order ¼ÆËã¹ı³Ì*/
+	for (order--; order >= 0; order--)
+		printf("%c", input[order]);
 	return 0;
 }
