27c17fd5e47cc8c59ffc1cb30ebee563dc13a619
哈哈哈 <1714302891@qq.com>
2024-08-11 20:00:30 +0800

    8.11.6

--- text.c
+++ text.c
@@ -2,18 +2,25 @@
 #include<stdio.h>
 int main(void)
 {
-	int group[8], order, number;
-	int input = 2;
-	for (order = 0, number = 1; order < 8;order++)
-	{
-		number *= 2;
-		group[order] = number;
-	}
-	order = 0;
-	do {
-		printf("%d ", group[order]);
-		order++;
-	} while (order < 8);
-	return 0;
+	float input[8], sum[8];
+	int order;
+	printf("Please enter eight number:");
+	for (order = 0; order < 8; order++)
+		scanf("%f", &input[order]);
 
+	
+		for (order = 0; order < 8; order++)
+			printf("%.2f ", input[order]);
+		printf("\n");
+		sum[0] = input[0];
+		printf("%.2f ", sum[0]);
+		for (order = 1; order < 8; order++)
+		{
+			sum[order] = sum[order - 1] + input[order];
+			printf("%.2f ", sum[order]);
+		}
+		scanf("%.2f", &input[order]);
+	
+	printf("\nDone!");
+	return 0;
 }
