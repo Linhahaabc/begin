be2e34fdccfe21b6adf2ef77028bd2b452ddfc99
哈哈哈 <1714302891@qq.com>
2024-08-11 20:00:03 +0800

    8.11.4

--- text.c
+++ text.c
@@ -1,21 +1,22 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<math.h>
 int main(void)
 {
-	int input[8], output[8];
-	int order;
-	printf("Please enter eight number:\n");
-	for (order = 0; order <= 7; order++)
-		scanf("%d", &input[order]);
-	for (order = 0; order <= 7; order++)
-		output[7 - order] = input[order];
-	for (order = 0; order <= 7; order++)
-		printf("%d ", output[order]);
-	printf("\nDone!\n");
+	int long cycle_index;
+	double number,common,sum1,sum2;
+	int num = 1;
+	printf("Please enter a number as the cycle index:");
+	scanf("%ld", &cycle_index);
+	printf("cycle_index             sum1            sum2\n");
+	for (number = 1.0, common = 1.0,sum1=0,sum2=0; number <= cycle_index; number++)
+	{
+		
+		sum1 += common / number;
+		sum2 += common / number * pow(-1,number-1);
+		printf("%11ld  %15f %15f\n",num , sum1, sum2);
+		num++;
+	}
+	printf("Done!\n");
 	return 0;
-
-	
-
-	
-
 }
