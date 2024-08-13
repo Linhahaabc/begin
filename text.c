1fc4478071ee603845219630bcf5287298cfe21f
哈哈哈 <1714302891@qq.com>
2024-08-13 15:25:29 +0800

    8.13.4

--- text.c
+++ text.c
@@ -1,39 +1,18 @@
-#define  _CRT_SECURE_NO_WARNINGS 1
+
 #include<stdio.h>
+#define a '.'
 int main(void)
 {
-	long input;
-	long num1, num2;
-	int judge = 1;
-
-	printf("Please enter a number:");
-	while (scanf("%ld", &input) == 1)
+	char ch;
+	int count=0;
+	
+	while ((ch = getchar()) != a)
 	{
+		if (ch != '" ' &&  ch != '\'')
+			count++;
 
-		for (num1 = 2; (num1 * num1) <= input; num1++)
-		{
-			if ((input % num1) == 0)
-			{
-				if (num1 * num1 == input)
-					printf("%ld is divisible by %ld\n", input, num1);
-				else
-				{
-					num2 = input / num1;
-					printf("%ld is divisible by %ld and %ld\n", input, num1, num2);
-				}
-				judge = 0;
-			}
-
-			
-		}
-
-		if (judge)
-		{
-			printf("%ld is prime\n", input);
-			
-		}
-		printf("Please enter a number:");
 	}
-	printf("Done!\n");
+	printf("There are %d letters.\n",count);
 	return 0;
 }
+
