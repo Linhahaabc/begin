e6b88168adb7b895cc9ee068cbecda440902c869
哈哈哈 <1714302891@qq.com>
2024-08-13 15:24:54 +0800

    8.13.2

--- text.c
+++ text.c
@@ -1,18 +1,38 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>,<ctype.h>
-
+#include<stdio.h>
+#include<stdbool.h>
 int main(void)
 {
-	char ch;
-	printf("Please enter some letter:");
-	while ((ch = getchar()) != '\n')
+	long long input,divide1,divide2;
+	bool isPrime;
+	printf("Please enter a number:");
+	
+	while (scanf("%lld", &input)==1)
 	{
-		if (isalpha(ch))
+		for (divide1 = 2,isPrime=true; divide1 * divide1 <= input; divide1++)
+		{
+			if((input%divide1)==0)
+			{
+				if (divide1 * divide1 != input)
+				{
+					divide2 = input / divide1;
+					printf("%lld is divisiable by %lld and %lld\n", input, divide1, divide2);
+				}
+				else
+					printf("%lld is divisiable by %lld \n", input, divide1);
 
-			putchar(ch + 1);
-		else
-			putchar(ch);
+				isPrime = false;
+			}
+			
+			
+			
+			
+		}
+		if (isPrime)
+			printf("%lld is prime.\n", input);
+		printf("Please enter a number:");
+		
 	}
+	printf("Done!\n");
 	return 0;
 }
-	
