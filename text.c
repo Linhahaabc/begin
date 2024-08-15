bf707a9dec7ccd8da2b4a532b8d6b8bedcb1ece8
哈哈哈 <1714302891@qq.com>
2024-08-15 13:01:59 +0800

    8.14.1

--- text.c
+++ text.c
@@ -1,18 +1,33 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#define a '.'
+#include<ctype.h>
+#include<stdbool.h>
+#define STOP '|'
 int main(void)
 {
 	char ch;
-	int count = 0;
+	int count=0;
+	long char1 = 0;
+	long words = 0;
+	long line = 0;
+	bool iswords = 0;
 
-	while ((ch=getchar())!=a)
+	printf("Please enter a article(use \" |\" to stop ):\n");
+	while ((ch = getchar()) != STOP)
 	{
-		if(ch!='"'&&ch!='\'')
-			count++;
+		char1++;
+		if (ch == '\n')
+			line++;
+		if (!isspace(ch) && !iswords)
+		{
+			words++;
+			iswords = true;
+		}
+		if (isspace(ch) && iswords)
+			iswords = false;
+
 
 	}
-	printf("%d", count);
+	printf("character is %ld,words is %ld,lines is %ld.", char1, words, line);
 	return 0;
 }
-
