ecb10049403f5c40fbbb4790b54779c70a6b7b0d
哈哈哈 <1714302891@qq.com>
2024-08-15 13:02:59 +0800

    8.4.3

--- text.c
+++ text.c
@@ -1,20 +1,35 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#define change 1000
+#define MAX 100.0
+#define MIN 0.0
 int main(void)
 {
-	int quantity,can;
-	
-	
-	printf("Please enter a number as quantities:");
-	while ((scanf("%d", &quantity) )== 1)
-	{
-		can = 0;
-		can = quantity / change;
-		can += (quantity % change == 0) ? 0 : 1;
-		printf("%d should use %d %s.",quantity,can,can==1?"can":"cans");
+	float min = MIN;
+	float max = MAX;
+	float total = 0.0;
+	float score;
+	int n=0;
 
+	printf("Please enter a number as your score(q to quit):");
+	while ((scanf("%f", &score)) == 1)
+	{
+		if (score <= MIN || score >= MAX)
+		{
+			printf("Your input is invaild\n");
+			printf("Please enter a number as your score(q to quit):");
+			continue;
+		}
+
+		n++;
+		total += score;
+		min = (score < max) ? score : max;
+		max = (score > min) ? score : min;
+		printf("Please enter a number as your score(q to quit):");
 
 	}
+	printf("The max score is %0.2f,min score is %0.2f.\n ", max, min);
+	printf("The total turn is %d,the total score is %0.2f,score per turn is %0.2f\n", n, total, total / n);
+	printf("Done!\n");
 	return 0;
+	
 }
